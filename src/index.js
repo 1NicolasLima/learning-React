be60f95bc5d7739814a6124aba439e454f52ce89
Nicolas Lima <nicolasaraujo845@gmail.com>
2022-11-26 09:18:25 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+import ReactDom from 'react-dom'
+import React from 'react'
+
+import Primeiro from './components/basicos/Primeiro'
+
+
+
+
+ReactDom.render(
+    <div>
+        <Primeiro></Primeiro>
+    </div>,
+    document.getElementById("root")
+)
